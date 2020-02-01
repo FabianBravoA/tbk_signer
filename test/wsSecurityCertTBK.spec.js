3df7690021f332ae033fdb2d88640bf5960e9497
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2020-01-31 23:30:22 -0300

    fix(test): New test with initTransaction passing

--- test/wsSecurityCertTBK.spec.js
+++ test/wsSecurityCertTBK.spec.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { createClient } from 'soap';
 import { tbkSecurityGenerator } from '../index';
 
+jest.setTimeout(10000);
 
 describe('TBK Signer tests | Se usan certificados de prueba', () => {
   const privateCert = fs.readFileSync(resolve('testCerts/privateTestCert.pem'), { encoding: 'utf-8' });
@@ -30,9 +31,57 @@ describe('TBK Signer tests | Se usan certificados de prueba', () => {
         )
         .then(done);
     });
+  });
 
-    it('Debería poder iniciar comunicación de prueba mediante SOAP', () => {
+  describe('Comprobar que permite enviar un mensaje SOAP usando el objeto de seguridad generado', () => {
+    it('Debería poder iniciar comunicación de prueba mediante SOAP - Simulación initTransaction', (done) => {
+      tbkSecurityGenerator(privateCert, publicCert)
+        .then(
+          (tbkSecurityForSoap) => {
+            createClient(
+              'https://webpay3gint.transbank.cl/WSWebpayTransaction/cxf/WSWebpayService?wsdl',
+              {
+                ignoredNamespaces: {
+                  namespaces: [],
+                  override: true,
+                },
+              },
+              (soapError, client) => {
+                expect(soapError).toBe(null);
+                client.setSecurity(tbkSecurityForSoap);
 
+                const testInput = {
+                  wsInitTransactionInput: {
+                    wSTransactionType: 'TR_NORMAL_WS',
+                    buyOrder: 'testOrder',
+                    sessionId: 1,
+                    returnURL: 'http://aTestUrl.com',
+                    finalURL: 'http://aFinalTestUrl.com',
+                    transactionDetails: {
+                      amount: 1000,
+                      commerceCode: 597020000541,
+                      buyOrder: 1,
+                    },
+                  },
+                };
+
+                client.WSWebpayServiceImplService.WSWebpayServiceImplPort.initTransaction(
+                  testInput,
+                  (error, result, raw) => {
+                    expect(error).toBe(null);
+                    expect(result).toBeDefined();
+                    expect(raw).toBeDefined();
+                    done();
+                  },
+                );
+              },
+            );
+          },
+        );
     });
   });
+});
+
+describe('TBK Verifier tests | Se verifican mensajes SOAP con certificado de prueba', () => {
+
 });
